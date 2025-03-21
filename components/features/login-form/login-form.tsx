52a00ed14b6369f40a934b8b751a12a1cac20f5e
Andrew Fisher <fisherandrew777@gmail.com>
2025-03-21 01:30:39 -0600

    Create Login Form

---
+++ components/features/login-form/login-form.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { Button, Divider } from 'thread-ui';
+import { authenticate } from '@/lib';
+import { LoginFormProps } from './login-form.types';
+
+export const LoginForm = ({ errorMessage }: LoginFormProps) => {
+    return (
+        <form
+            action={authenticate}
+            className="sm:w-10/12 md:max-w-80 mx-auto flex flex-col gap-6 justify-center items-center border rounded max-w-screen-sm p-4"
+        >
+            <span>
+                Admin Login
+                <Divider />
+            </span>
+            <label className="flex flex-col gap-2 w-full">
+                Email
+                <input className={`border rounded p-2`} id="email" name="email" type="email" required minLength={2} />
+            </label>
+
+            <label className="flex flex-col gap-2 w-full">
+                Password
+                <input
+                    className={`border rounded p-2 ${errorMessage ? 'border-red-500' : ''}`}
+                    id="password"
+                    name="password"
+                    type="password"
+                    required
+                    minLength={2}
+                />
+                {errorMessage && <span className="text-red-500 text-sm">{errorMessage}</span>}
+            </label>
+
+            <Button fullWidth type="submit">
+                Submit
+            </Button>
+        </form>
+    );
+};
---
+++ components/features/login-form/login-form.types.ts
@@ -0,0 +1,3 @@
+export type LoginFormProps = {
+    errorMessage?: string;
+};
