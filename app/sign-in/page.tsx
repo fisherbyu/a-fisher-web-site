ab3f0581f206b1a2b363e4f05d99ff77268dfce5
Andrew Fisher <fisherandrew777@gmail.com>
2025-03-21 01:39:58 -0600

    Start Login

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,6 @@
+import { LoginForm } from '@/components';
+
+export default async function Login(props: { searchParams: Promise<string> }) {
+    const searchParams = await props.searchParams;
+    return <LoginForm />;
+}
