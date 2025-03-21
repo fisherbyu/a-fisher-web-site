2ab614a7a7905e0040dcd2fc5b6434429c0dc229
Andrew Fisher <fisherandrew777@gmail.com>
2025-03-21 01:48:01 -0600

    Update signin

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -3,5 +3,4 @@ import { LoginForm } from '@/components';
 export default async function Login(props: { searchParams: Promise<string> }) {
     const searchParams = await props.searchParams;
     return <LoginForm />;
-    return <>h</>;
 }
