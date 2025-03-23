829a2797416f48f1815d0db4f911e4d47467ac1c
Andrew Fisher <fisherandrew777@gmail.com>
2025-03-23 15:11:24 -0600

    Update Form

--- components/features/login-form/login-form.tsx
+++ components/features/login-form/login-form.tsx
@@ -1,12 +1,36 @@
 'use client';
-
 import { Button, Divider } from 'thread-ui';
+import { authenticateUser } from '@/lib';
+import { useRouter } from 'next/navigation';
 
 export const LoginForm = () => {
-    const errorMessage = true;
+    const router = useRouter();
+    // const [errorMessage, setErrorMessage] = useState('');
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target as HTMLFormElement);
+        const email = formData.get('email')!.toString();
+        const password = formData.get('password')!.toString();
+
+        try {
+            const result = await authenticateUser(email, password);
+
+            if (result.success) {
+                router.push('/admin');
+            } else {
+                // setErrorMessage(result.error || 'Authentication failed');
+                console.error('Authentication Failed:', result.error);
+            }
+        } catch (err) {
+            console.error('Authentication Failed:', err);
+        }
+    };
+
     return (
         <form
-            action={() => null}
+            onSubmit={handleSubmit}
             className="sm:w-10/12 md:max-w-80 mx-auto flex flex-col gap-6 justify-center items-center border rounded max-w-screen-sm p-4"
         >
             <span>
@@ -15,24 +39,14 @@ export const LoginForm = () => {
             </span>
             <label className="flex flex-col gap-2 w-full">
                 Email
-                <input className={`border rounded p-2`} id="email" name="email" type="email" required minLength={2} />
+                <input className="border rounded p-2" id="email" name="email" type="email" required minLength={2} />
             </label>
-
             <label className="flex flex-col gap-2 w-full">
                 Password
-                <input
-                    className={`border rounded p-2 ${errorMessage ? 'border-red-500' : ''}`}
-                    id="password"
-                    name="password"
-                    type="password"
-                    required
-                    minLength={2}
-                />
-                {errorMessage && <span className="text-red-500 text-sm">{errorMessage}</span>}
+                <input className={`border rounded p-2 `} id="password" name="password" type="password" required minLength={2} />
             </label>
-
             <Button fullWidth type="submit">
-                Submit
+                Login
             </Button>
         </form>
     );
