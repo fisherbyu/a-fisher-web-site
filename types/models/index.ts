df7a71d766629d5f1303b59f7382fcf85b35dbd6
Andrew Fisher <fisherandrew777@gmail.com>
2025-03-24 10:42:15 -0600

    Add Order to tags

--- types/models/index.ts
+++ types/models/index.ts
@@ -57,11 +57,13 @@ export interface Content {
  * @property {string} id - Tag ID
  * @property {string} title - Tag title
  * @property {string} content - Tag content description
+ * @property {number} order - Order of Tag
  */
 export interface Tag {
     id: string;
     title: string;
     content: string;
+    order: number;
 }
 
 /**
