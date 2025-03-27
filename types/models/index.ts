df235322373ac3a29716ad727e74a4785dd5e65e
Andrew Fisher <fisherandrew777@gmail.com>
2025-03-27 14:38:06 -0600

    Begin model refactor

--- types/models/index.ts
+++ types/models/index.ts
@@ -1,7 +1,58 @@
-// Export Models
-export * from './artist';
-export * from './album';
-export * from './content';
-export * from './image';
-export * from './music-link';
-export * from './tag';
+// artist.ts
+export type Artist = {
+    id: string;
+    name: string;
+    tier: number;
+    rank?: number;
+    link: Link;
+    image: Image;
+    contents: ContentBlock[];
+    attributes: Attribute[];
+};
+
+// album.ts
+export type Album = {
+    id: string;
+    name: string;
+    rank: number;
+    link: Link;
+    image: Image;
+    contents: ContentBlock[];
+    attributes: Attribute[];
+};
+
+// playlist.ts
+export type Playlist = {
+    id: string;
+    title: string;
+    link: Link;
+};
+
+// link.ts
+export type Link = {
+    id: string;
+    appleURI: string;
+    spotifyURI: string;
+};
+
+// image.ts
+export type Image = {
+    id: string;
+    src: string;
+    alt: string;
+};
+
+// content.ts
+export type ContentBlock = {
+    id: string;
+    content: string;
+    order: number;
+};
+
+// attribute.ts
+export type Attribute = {
+    id: string;
+    title: string;
+    content: string;
+    order: number;
+};
