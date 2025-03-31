a37c989f83b6710c56593b9f4397bb070aacbd0b
Andrew Fisher <fisherandrew777@gmail.com>
2025-03-31 16:25:51 -0600

    Begin building admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,13 @@
-import { SignOutButton } from '@/components';
+import { SignOutButton, ArtistForm } from '@/components';
 
 export default async function Admin() {
     return (
-        <>
-            <h1>Hello AF</h1>
-            <SignOutButton />
-        </>
+        <div className="w-full h-full py-5 px-8 flex flex-col">
+            <div className="w-full md:w-11/12 mx-auto flex flex-row items-center justify-between">
+                <h1>Admin</h1>
+                <SignOutButton />
+            </div>
+            <ArtistForm />
+        </div>
     );
 }
