6abefcc4bb22f877c041ba85b3cf4ee752ff7709
Andrew Fisher <fisherandrew777@gmail.com>
2025-03-31 16:37:30 -0600

    Update dropdown

--- components/features/forms/dropdown.tsx
+++ components/features/forms/dropdown.tsx
@@ -3,11 +3,9 @@ import { CaretDown, CaretUp } from '@phosphor-icons/react';
 import { Icon } from 'thread-ui';
 
 interface DropdownProps {
-    id: string;
     label: string;
     value: string | number;
     options: DropdownOption[];
-    required?: boolean;
     onSelect: (value: string | number) => void;
 }
 
@@ -16,7 +14,7 @@ interface DropdownOption {
     value: string | number;
 }
 
-export const Dropdown = ({ id, label, value, options, required, onSelect }: DropdownProps) => {
+export const Dropdown = ({ label, value, options, onSelect }: DropdownProps) => {
     const [isOpen, setIsOpen] = useState(false);
     const [selected, setSelected] = useState(options[0]);
 
@@ -29,41 +27,24 @@ export const Dropdown = ({ id, label, value, options, required, onSelect }: Drop
     };
 
     return (
-        <div>
-            <label htmlFor={id} className="block font-medium text-gray-700">
-                {label}
-            </label>
-            <select
-                id={id}
-                name={id}
-                required={required}
-                value={value}
-                className="w-full bg-white border border-gray-300 rounded-md mt-1 shadow-lg"
+        <div className="w-full">
+            <div className="block font-medium text-gray-700 ">{label}</div>
+            <button
+                className="flex justify-between items-center w-full px-4 py-2 bg-gray-100 rounded-lg shadow-sm focus:outline-none"
+                onClick={toggleDropdown}
             >
-                {options.map((option) => (
-                    <option key={option.label} value={option.value} className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
-                        {option.label}
-                    </option>
-                ))}
-            </select>
+                <span>{selected.label}</span>
+                {isOpen ? <Icon name="CaretUp" size={16} color="black" /> : <Icon name="CaretDown" size={16} color="black" />}
+            </button>
+            {isOpen && (
+                <ul className=" w-full bg-white border border-gray-300 rounded-md mt-1 shadow-lg">
+                    {options.map((option, index) => (
+                        <li key={index} className="px-4 py-2 hover:bg-gray-100 cursor-pointer" onClick={() => handleSelect(option)}>
+                            {option.label}
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
-        // <div className="w-full">
-        //     <button
-        //         className="flex justify-between items-center w-full px-4 py-2 bg-gray-100 rounded-lg shadow-sm focus:outline-none"
-        //         onClick={toggleDropdown}
-        //     >
-        //         <span>{selected}</span>
-        //         {isOpen ? <Icon name="CaretUp" size={16} color="black" /> : <Icon name="CaretDown" size={16} color="black" />}
-        //     </button>
-        //     {isOpen && (
-        //         <ul className=" w-full bg-white border border-gray-300 rounded-md mt-1 shadow-lg">
-        //             {options.map((option) => (
-        //                 <li key={option} className="px-4 py-2 hover:bg-gray-100 cursor-pointer" onClick={() => handleSelect(option)}>
-        //                     {option}
-        //                 </li>
-        //             ))}
-        //         </ul>
-        //     )}
-        // </div>
     );
 };
