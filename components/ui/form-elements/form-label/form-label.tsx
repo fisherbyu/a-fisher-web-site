2eef309b95a585e01ce383d2296c2038764179e0
Andrew Fisher <fisherandrew777@gmail.com>
2025-04-01 00:53:09 -0600

    Enforce margins

--- components/ui/form-elements/form-label/form-label.tsx
+++ components/ui/form-elements/form-label/form-label.tsx
@@ -2,7 +2,7 @@ import { FormLabelProps } from './form-label.types';
 
 export const FormLabel = ({ name, id = name, title }: FormLabelProps) => {
     return (
-        <label id={id} htmlFor={name} className="font-medium text-gray-700 mb-2">
+        <label id={id} htmlFor={name} className="block font-medium text-gray-700 mb-2">
             {title}
         </label>
     );
