e2b4a2bff9a13b104d65bc6ec9a3ee2cc5fd91d6
Andrew Fisher <fisherandrew777@gmail.com>
2025-04-01 03:04:36 -0600

    Make full width

--- components/ui/form-elements/input-styles.ts
+++ components/ui/form-elements/input-styles.ts
@@ -1,5 +1,5 @@
 const BASE_STYLES = {
-    width: 'w-10/12',
+    width: 'w-full',
     layout: 'px-4 py-2',
     border: 'border border-gray-300 rounded-md',
     focus: 'focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-colors',
