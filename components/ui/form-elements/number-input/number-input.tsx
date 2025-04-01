10f845d72827461453e53fff3d8a7e1ed3ac3f1e
Andrew Fisher <fisherandrew777@gmail.com>
2025-04-01 00:22:14 -0600

    Create NumberInput

--- components/ui/form-elements/number-input/number-input.tsx
+++ components/ui/form-elements/number-input/number-input.tsx
@@ -1,3 +1,19 @@
+import { FormLabel } from '../form-label';
+import { INPUT_STYLES } from '../input-styles';
 import { NumberInputProps } from './number-input.types';
 
-export const NumberInput = () => {};
+export const NumberInput = ({ name, id = name, title, value, required, onChange, min }: NumberInputProps) => {
+    <div>
+        <FormLabel id={id} name={name} title={title} />
+        <input
+            type="number"
+            id={id}
+            name={name}
+            required={required}
+            value={value}
+            onChange={onChange}
+            className={INPUT_STYLES.standard}
+            min={min}
+        />
+    </div>;
+};
