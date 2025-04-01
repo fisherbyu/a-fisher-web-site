08f7eb75710aa246d0adc7a6cd6ebb374062e01e
Andrew Fisher <fisherandrew777@gmail.com>
2025-04-01 00:00:25 -0600

    Create Style Constants

---
+++ components/ui/form-elements/input-styles.ts
@@ -0,0 +1,10 @@
+const BASE_STYLES = {
+    layout: 'px-4 py-2 ',
+    border: 'border border-gray-300 rounded-md',
+    focus: 'focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-colors',
+    darkBg: '',
+};
+
+export const INPUT_STYLES = {
+    standard: BASE_STYLES.border + BASE_STYLES.focus + BASE_STYLES.layout,
+};
