1ecef8b4cf5243bc82edb01e11e83f04844182bc
Andrew Fisher <fisherandrew777@gmail.com>
2025-04-01 00:27:01 -0600

    Return JSX

--- components/ui/form-elements/number-input/number-input.tsx
+++ components/ui/form-elements/number-input/number-input.tsx
@@ -3,17 +3,19 @@ import { INPUT_STYLES } from '../input-styles';
 import { NumberInputProps } from './number-input.types';
 
 export const NumberInput = ({ name, id = name, title, value, required, onChange, min }: NumberInputProps) => {
-    <div>
-        <FormLabel id={id} name={name} title={title} />
-        <input
-            type="number"
-            id={id}
-            name={name}
-            required={required}
-            value={value}
-            onChange={onChange}
-            className={INPUT_STYLES.standard}
-            min={min}
-        />
-    </div>;
+    return (
+        <div>
+            <FormLabel id={id} name={name} title={title} />
+            <input
+                type="number"
+                id={id}
+                name={name}
+                required={required}
+                value={value}
+                onChange={onChange}
+                className={INPUT_STYLES.standard}
+                min={min}
+            />
+        </div>
+    );
 };
