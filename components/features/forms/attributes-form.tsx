4240193878b4c1a455f560a71101049f44a40194
Andrew Fisher <fisherandrew777@gmail.com>
2025-04-02 12:48:02 -0600

    Use Editable List Item

--- components/features/forms/attributes-form.tsx
+++ components/features/forms/attributes-form.tsx
@@ -1,4 +1,5 @@
-import { ReorderableList } from '@/components/ui';
+import { ReorderableList, TextInput } from '@/components/ui';
+import { EditableListItem } from '@/components/ui/form-elements/editable-list-item';
 import { Attribute } from '@/types';
 import { ReactNode } from 'react';
 
@@ -13,13 +14,21 @@ type EditAttributeProps = Omit<Attribute, 'id'> & {
 const EditAttribute = (props: EditAttributeProps) => {
     const { title, text, order, dragHandle } = props;
 
-    return (
-        <div>
-            {dragHandle}
-            {title}
-            {text}
+    const displayAttribute = (
+        <div className="flex flex-col justify-start">
+            <h3 className="font-medium">{title}</h3>
+            <p className=" font-light">{text}</p>
         </div>
     );
+
+    const editAttributeData = (
+        <div className="flex flex-col justify-start">
+            <TextInput name={`title-${title}`} title={'Title'} value={title} required />
+            <TextInput name={`title-${text}`} title={'Text'} value={text} required />
+        </div>
+    );
+
+    return <EditableListItem dragHandle={dragHandle} display={displayAttribute} edit={editAttributeData} />;
 };
 
 export const AttributesForm = ({ data, onChange }: AttributesFormProps) => {
