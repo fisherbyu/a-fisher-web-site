82c251db07071bb42e3a2973532a677cdb0d0aa5
Andrew Fisher <fisherandrew777@gmail.com>
2025-04-02 12:02:16 -0600

    Init Attributes Form

---
+++ components/features/forms/attributes-form.tsx
@@ -0,0 +1,9 @@
+import { Attribute } from '@/types';
+
+type AttributesFormProps = {
+    data: Attribute[];
+    onChange: (data: Attribute[]) => void;
+};
+export const AttributesForm = ({ data, onChange }: AttributesFormProps) => {
+    return <></>;
+};
