83a7396e5f1084ba0027c3fe3469db4c324ee6b1
Andrew Fisher <fisherandrew777@gmail.com>
2025-04-02 12:09:43 -0600

    Create Basic Structure

--- components/features/forms/attributes-form.tsx
+++ components/features/forms/attributes-form.tsx
@@ -1,9 +1,32 @@
+import { ReorderableList } from '@/components/ui';
 import { Attribute } from '@/types';
+import { ReactNode } from 'react';
 
 type AttributesFormProps = {
     data: Attribute[];
     onChange: (data: Attribute[]) => void;
 };
+
+type EditAttributeProps = Omit<Attribute, 'id'> & {
+    dragHandle: ReactNode;
+};
+const EditAttribute = (props: EditAttributeProps) => {
+    const { title, text, order, dragHandle } = props;
+
+    return (
+        <div>
+            {dragHandle}
+            {title}
+            {text}
+        </div>
+    );
+};
+
 export const AttributesForm = ({ data, onChange }: AttributesFormProps) => {
-    return <></>;
+    return (
+        <div>
+            <h1>Contents</h1>
+            <ReorderableList data={data} orderProperty="order" ItemComponent={EditAttribute} onChange={onChange} />
+        </div>
+    );
 };
