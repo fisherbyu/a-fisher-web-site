e83a204219183fc4bc98e3af602852b5c0c27537
Andrew Fisher <fisherandrew777@gmail.com>
2025-04-02 14:22:10 -0600

    Add minimum

--- components/features/forms/artist-info-form.tsx
+++ components/features/forms/artist-info-form.tsx
@@ -29,7 +29,7 @@ export const ArtistInfoForm = ({ data, onChange }: ArtistInfoFormProps) => {
     return (
         <div>
             <TextInput name="name" title="Name" value={data.name} onChange={handleChange} required />
-            <NumberInput name="rank" title="Rank" value={data.rank} onChange={handleChange} required />
+            <NumberInput name="rank" title="Rank" value={data.rank} onChange={handleChange} required min={1} />
             <Dropdown label="Tier" value={data.tier} options={tierOptions} onSelect={() => null} />
         </div>
     );
