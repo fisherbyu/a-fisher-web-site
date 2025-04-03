aa71fad33267dd0090ac75b0e4d4f2f3e400eab6
Andrew Fisher <fisherandrew777@gmail.com>
2025-04-03 02:06:27 -0600

    Use omit on model

--- components/models/attribute/edit-attribute-data/edit-attribute.types.ts
+++ components/models/attribute/edit-attribute-data/edit-attribute.types.ts
@@ -1,5 +1,5 @@
-export type EditAttributeDataProps = {
-    title: string;
-    text: string;
+import { AttributeDto } from '@/types';
+
+export type EditAttributeDataProps = Omit<AttributeDto, 'id' | 'order'> & {
     onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => void;
 };
