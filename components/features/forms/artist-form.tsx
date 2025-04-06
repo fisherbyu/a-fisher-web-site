c6458ae5c434fe85d0c7a70c4f7e6aefe7629897
Andrew Fisher <fisherandrew777@gmail.com>
2025-04-06 15:18:12 -0600

    Implement change and state functions

--- components/features/forms/artist-form.tsx
+++ components/features/forms/artist-form.tsx
@@ -1,13 +1,12 @@
 'use client';
-import { Divider } from 'thread-ui';
-import { ArtistInfoForm } from './artist-info-form';
-import { Artist, ArtistDto, Attribute, AttributeDto, Content, ContentDto, Image, Link } from '@/types';
+import { Button, Divider } from 'thread-ui';
+import { ArtistInfoData, ArtistInfoForm } from './artist-info-form';
+import { Artist, ArtistDto, Attribute, AttributeDto, Content, ContentDto, Image, ImageDto, Link, LinkDto } from '@/types';
 import { useId, useState } from 'react';
 import { FileUpload } from '@/components';
 import { LinkForm } from './link-form';
 import { ContentData, ContentsForm } from './contents-form';
 import { AttributesForm } from '@/components';
-import { handleArtist } from '@/lib/hooks/use-entity-state/handleArtist';
 
 type FormProps = {
     initialData?: Artist;
@@ -15,77 +14,49 @@ type FormProps = {
 };
 
 export const ArtistForm = ({ initialData, onSuccess }: FormProps) => {
-    const [contents, setContents] = useState<Content[]>([
-        { id: 0, order: 1, text: 'Test' },
-        { id: 1, order: 2, text: '' },
-    ]);
-    const artistHandler = handleArtist<Artist, ArtistDto>({
-        data: initialData,
-        dto: {
-            id: useId(),
-            name: '',
-            tier: 0,
-            contents: [],
-            attributes: [],
-        },
-        createEntity: (data: ArtistDto) => {},
-        saveEntity: (data: Artist) => {},
-        attributeHandlers: {
-            contents: {
-                onChange: setContents,
-            },
-        },
-        attributeCreators: {},
-    });
-
-    // Handle core Artist Details
-    const [artistInfo, setArtistInfo] = useState({
+    // Extract or Init Data
+    // Artist Info
+    const [artistInfo, setArtistInfo] = useState<ArtistInfoData>({
         name: initialData?.name || '',
-        tier: initialData?.tier || 1,
-        rank: initialData?.rank || undefined,
+        tier: initialData?.tier,
+        rank: initialData?.rank,
     });
 
-    // Handle Link Info
-    const [link, setLink] = useState<Omit<Link, 'id'>>({
-        appleURI: initialData?.link?.appleURI || '',
-        spotifyURI: initialData?.link?.spotifyURI || '',
-    });
+    // Contents
+    const [contents, setContents] = useState<Array<Content | ContentDto>>(initialData?.contents || []);
+    const addContent = () => {
+        const newContent: ContentDto = {
+            id: crypto.randomUUID(),
+            text: '',
+            order: contents.length,
+        };
+        setContents([...contents, newContent]);
+    };
 
-    // Handle Image Data
-    const [image, setImage] = useState<Omit<Image, 'id'>>({
-        src: initialData?.image?.src || '',
-        alt: initialData?.image?.alt || '',
-        height: initialData?.image?.height || 0,
-        width: initialData?.image?.width || 0,
-    });
+    // Attributes
+    const [attributes, setAttributes] = useState<Array<Attribute | AttributeDto>>(initialData?.attributes || []);
+    const addAttribute = () => {
+        const newAttribute: AttributeDto = {
+            id: crypto.randomUUID(),
+            title: '',
+            text: '',
+            order: attributes.length,
+        };
+        setAttributes([...attributes, newAttribute]);
+    };
+
+    // Link
+    const [link, setLink] = useState<Link | LinkDto>(initialData?.link || { id: useId(), appleURI: '', spotifyURI: '' });
+
+    // Image
+    const [image, setImage] = useState<Image | ImageDto>(initialData?.image || { id: useId(), src: '', alt: '', height: 0, width: 0 });
     const handleFileSelect = async (file: File): Promise<void> => {
         console.log('IMG PROCESS:', file.name);
     };
 
-    // Handle Content Data
-
-    const updateContents = (data: ContentData[]) => {
-        const newData = data.map(({ id, ...rest }) => ({
-            id: typeof id === 'string' ? parseInt(id, 10) : id,
-            ...rest,
-        }));
-
-        setContents(newData);
-    };
-
-    // Handle Attribute Data
-    // const [attributes, setAttributes] = useState<Attribute[]>(
-    //     initialData?.attributes || [{ id: 1, order: 1, title: 'Title', text: 'Contents' }]
-    // );
-    const [attributes, setAttributes] = useState<AttributeDto[]>([{ id: '1', order: 1, title: 'Title', text: 'Contents' }]);
-
-    const updateAttributes = (data: AttributeDto[]) => {
-        // const newData = data.map(({ id, ...rest }) => ({
-        //     id: typeof id === 'string' ? parseInt(id, 10) : id,
-        //     ...rest,
-        // }));
-
-        setAttributes(data);
+    // Submission
+    const handleSubmit = () => {
+        //
     };
 
     return (
@@ -98,8 +69,8 @@ export const ArtistForm = ({ initialData, onSuccess }: FormProps) => {
                     <LinkForm data={link} onChange={setLink} />
                 </div>
                 <div>
-                    <AttributesForm data={attributes} onChange={updateAttributes} />
-                    <ContentsForm data={contents} onChange={updateContents} />
+                    <AttributesForm data={attributes} onChange={setAttributes} onAdd={addAttribute} />
+                    <ContentsForm data={contents} onChange={setContents} onAdd={addContent} />
                 </div>
                 <FileUpload
                     title="Add Image"
@@ -108,6 +79,7 @@ export const ArtistForm = ({ initialData, onSuccess }: FormProps) => {
                     onFileSelect={handleFileSelect}
                 />
             </div>
+            <Button onClick={handleSubmit}>Submit</Button>
         </form>
     );
 };
