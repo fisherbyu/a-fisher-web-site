64fe4f3744f1d371abb5fdb19aca6b8b21f6d731
Andrew Fisher <fisherandrew777@gmail.com>
2025-04-07 12:40:20 -0600

    Create Artist Page

---
+++ app/admin/artist/page.tsx
@@ -0,0 +1,9 @@
+import { ArtistForm } from '@/components';
+
+export default async function Admin() {
+    return (
+        <div className="w-full h-full py-5 px-8 flex flex-col">
+            <ArtistForm />
+        </div>
+    );
+}
