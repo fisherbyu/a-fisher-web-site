c7de283e6fc1c5706e73edd4edf56f1936b6a961
Andrew Fisher <fisherandrew777@gmail.com>
2025-04-07 00:59:53 -0600

    Create getArtists()

---
+++ lib/actions/artist/get-artists.ts
@@ -0,0 +1,23 @@
+import { prisma } from '@/lib/prisma';
+import { transformArtist } from '@/lib';
+
+export const getArtists = async () => {
+    const data = await prisma.artist.findMany({
+        include: {
+            link: true,
+            image: true,
+            contents: {
+                orderBy: {
+                    order: 'asc',
+                },
+            },
+            attributes: {
+                orderBy: {
+                    order: 'asc',
+                },
+            },
+        },
+    });
+
+    return data.map((artist) => transformArtist(artist));
+};
--- lib/actions/artist/index.ts
+++ lib/actions/artist/index.ts
@@ -1,1 +1,2 @@
 export { createArtist } from './create-artist';
+export { getArtists } from './get-artists';
