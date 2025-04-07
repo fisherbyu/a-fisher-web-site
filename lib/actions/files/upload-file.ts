c6f0b67933978b6a54078fd9a48bdb3bcbde5f0e
Andrew Fisher <fisherandrew777@gmail.com>
2025-04-06 18:25:36 -0600

    File Upload Functionality

---
+++ lib/actions/files/upload-file.ts
@@ -0,0 +1,25 @@
+import { BaseResponse, Image } from '@/types';
+import { createClient } from '@/lib';
+
+type UploadFileProps = {
+    file: File;
+    filePath: string;
+};
+
+export async function uploadFile({ file, filePath }: UploadFileProps): Promise<{
+    file: File;
+    path: string;
+}> {
+    // Init Supabase Client
+    const supabase = createClient();
+
+    // Upload File to Supabase
+    const BUCKET_NAME = process.env.BUCKET_NAME!;
+    const { data, error } = await supabase.storage.from(BUCKET_NAME).upload(filePath, file);
+    if (error) {
+        console.log('Error Uploading File');
+        throw error;
+    } else {
+        return { file: file, path: data.path };
+    }
+}
