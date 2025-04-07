692303286a553079b95756031b87345b132e1f95
Andrew Fisher <fisherandrew777@gmail.com>
2025-04-06 22:03:33 -0600

    Use utility for Icon display

--- components/ui/form-elements/file-upload/previews/file-preview/file-preview.tsx
+++ components/ui/form-elements/file-upload/previews/file-preview/file-preview.tsx
@@ -1,10 +1,11 @@
 import { Icon } from 'thread-ui';
 import { FilePreviewProps } from './file-preview.types';
+import { isFileImageType } from '@/lib';
 
-export const FilePreview = ({ file, type }: FilePreviewProps) => {
+export const FilePreview = ({ file }: FilePreviewProps) => {
     return (
         <div className="w-full flex items-center justify-between rounded bg-gray-100 py-4 px-5">
-            <Icon name={type === 'IMG' ? 'Image' : 'FileText'} size={48} color="grey" />
+            <Icon name={isFileImageType(file) ? 'Image' : 'FileText'} size={48} color="grey" />
             <p className="mt-2 text-sm font-medium">{file.name}</p>
             <p className="text-xs text-gray-500">{(file.size / 1024).toFixed(1)} KB</p>
         </div>
