e410797f50267403828e851c0a8dbb527aac597c
Andrew Fisher <fisherandrew777@gmail.com>
2025-04-07 00:33:12 -0600

    Fix Image Processins

--- components/features/forms/artist-form.tsx
+++ components/features/forms/artist-form.tsx
@@ -5,9 +5,11 @@ import { Artist, ArtistDto, Attribute, AttributeDto, Content, ContentDto, Image,
 import { useId, useState } from 'react';
 import { FileUpload } from '@/components';
 import { LinkForm } from './link-form';
-import { ContentData, ContentsForm } from './contents-form';
+import { ContentsForm } from './contents-form';
 import { AttributesForm } from '@/components';
 import { createArtist } from '@/lib/actions/artist';
+import { uploadImage } from '@/lib';
+import { FileWithAlt } from '@/components/ui/form-elements/file-upload/file-upload.types';
 
 type FormProps = {
     initialData?: Artist;
@@ -19,7 +21,7 @@ export const ArtistForm = ({ initialData, onSuccess }: FormProps) => {
     // Artist Info
     const [artistInfo, setArtistInfo] = useState<ArtistInfoData>({
         name: initialData?.name || '',
-        tier: initialData?.tier,
+        tier: initialData?.tier || 1,
         rank: initialData?.rank,
     });
 
@@ -50,27 +52,34 @@ export const ArtistForm = ({ initialData, onSuccess }: FormProps) => {
     const [link, setLink] = useState<Link | LinkDto>(initialData?.link || { id: useId(), appleURI: '', spotifyURI: '' });
 
     // Files
-    const [files, setFiles] = useState<File[]>([]);
+    const [files, setFiles] = useState<FileWithAlt[]>([]);
 
     // Image
     const [image, setImage] = useState<Image | ImageDto>(initialData?.image || { id: useId(), src: '', alt: '', height: 0, width: 0 });
-    const handleFileSelect = async (file: File): Promise<void> => {
-        console.log('IMG PROCESS:', file.name);
-    };
 
     // Submission
-    const handleSubmit = () => {
+    const handleSubmit = async () => {
         // Edit Artist
         if (initialData) {
             console.log(initialData);
         }
         // Create Artist
-        else {
+        else if (files[0]) {
             try {
+                const file = files[0];
+                const newImage = await uploadImage({
+                    file: file,
+                    alt: file.alt || '',
+                    filePath: 'music/artists/',
+                });
+                setImage({
+                    id: image.id,
+                    ...newImage,
+                });
                 const dto: ArtistDto = {
                     id: crypto.randomUUID(),
                     name: artistInfo.name,
-                    tier: artistInfo.tier!,
+                    tier: artistInfo.tier,
                     rank: artistInfo.rank,
                     contents: contents,
                     attributes: attributes,
@@ -103,6 +112,7 @@ export const ArtistForm = ({ initialData, onSuccess }: FormProps) => {
                     supportedFormatsText="Supports all Image Types"
                     files={files}
                     setFiles={setFiles}
+                    maxNumberFiles={1}
                 />
             </div>
             <Button onClick={handleSubmit}>Submit</Button>
