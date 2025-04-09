be3833bc5275440810737f33002f71ace068fdfc
Andrew Fisher <fisherandrew777@gmail.com>
2025-04-09 16:53:36 -0600

    Make root layout generic

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,6 @@ import '@/public/styles/globals.css';
 import type { Metadata } from 'next';
 import { Merriweather_Sans } from 'next/font/google';
 
-import { CoreMenu } from '@/components/old/core/core-menu';
-import CoreFooter from '@/components/old/core/core-footer';
-
 // Configure Site
 const MAIN_FONT = Merriweather_Sans({ subsets: ['latin'] });
 
@@ -20,11 +17,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" style={MAIN_FONT.style}>
-            <body className="flex flex-col min-h-screen">
-                <CoreMenu />
-                <main className="flex-grow">{children}</main>
-                <CoreFooter />
-            </body>
+            <body>{children}</body>
         </html>
     );
 }
