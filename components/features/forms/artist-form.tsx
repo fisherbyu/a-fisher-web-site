c127142d92aed756e49ab9e7a45ccc1cfd6220be
Andrew Fisher <fisherandrew777@gmail.com>
2025-04-09 13:24:48 -0600

    Change size

--- components/features/forms/artist-form.tsx
+++ components/features/forms/artist-form.tsx
@@ -119,7 +119,7 @@ export const ArtistForm = ({ initialData, onSuccess }: FormProps) => {
                             files={files}
                             setFiles={setFiles}
                             maxNumberFiles={1}
-                            size="sm"
+                            size="md"
                         />
                     )}
                 </div>
