d5ffad2c25adf7708782b88308c17adc007d87cf
Andrew Fisher <fisherandrew777@gmail.com>
2025-05-01 00:51:28 -0600

    Implement Panel

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -13,6 +13,7 @@ import Philippines from '@/public/home/philippines.jpg';
 // Import Components
 import { Separator } from '@/components/old/ui/separator';
 import GridBlock from '@/components/old/ui/grid-block';
+import { ColumnLayout, ImagePanel, Text, Title } from 'thread-ui';
 
 // Declare Content
 const places: {
@@ -96,51 +97,19 @@ const movies: {
     ],
 };
 
-// Hero Header
-function HeroBlock() {
-    return (
-        <section className="container">
-            <div className="flex flex-col lg:flex-row justify-center items-stretch">
-                <div id="img block" className="mt-6 md:mt-8 lg:mt-0 lg:w-8/12 ">
-                    <div className="relative w-full h-full">
-                        <Image
-                            src={ImageAF_3_2}
-                            alt="Picture of Andrew Fisher"
-                            className="w-full h-full relative hidden lg:block rounded"
-                        />
-                        <Image
-                            src={ImageAF_5_4}
-                            alt="Picture of Andrew Fisher"
-                            className="md:mx-auto w-full h-full lg:hidden rounded"
-                        />
-                    </div>
-                </div>
-                <div
-                    id="text block"
-                    className="flex justify-center items-center order-first md:mx-auto md:w-11/12 md:mt-12 lg:w-5/12 lg:px-6 lg:order-none"
-                >
-                    <div>
-                        <h1 className=" dark:text-white text-4xl md:text-5xl xl:text-6xl font-semibold text-gray-900 w-10/12">
-                            I'm Andrew!
-                        </h1>
-                        <p className="dark:text-gray-300 md:w-10/12 lg:w-11/12 xl:w-10/12 mt-4 lg:mt-5 text-base leading-normal text-gray-600">
-                            I'm a Master of Information Systems Managment Student at BYU interested in software development. I love
-                            serving and getting to know people and using technology to create powerful solutions. I love music and
-                            music history and am an avid cook and baker. Thanks for visiting my site!
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
-}
-
 // Content
 export default function Home() {
     return (
         <main>
             <br />
-            <HeroBlock />
+            <ImagePanel
+                title="I'm Andrew!"
+                contents={[
+                    "I'm a Master of Information Systems Managment Student at BYU interested in software development. I love serving and getting to know people and using technology to create powerful solutions. I love music and music history and am an avid cook and baker. Thanks for visiting my site!",
+                ]}
+                image={<Image src={ImageAF_3_2} alt="Picture of Andrew Fisher" />}
+                smImage={<Image src={ImageAF_5_4} alt="Picture of Andrew Fisher" />}
+            />
             <Separator className="container w-9/12 my-9" />
             <h1 className="container font-semibold md:py-6 lg:py-3 text-center text-3xl md:text-4xl xl:text-5xl">About Me</h1>
             <GridBlock components={places} />
