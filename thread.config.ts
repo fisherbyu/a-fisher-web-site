694f40317e68711d5124f2afde224c46406a0dba
Andrew Fisher <fisherandrew777@gmail.com>
2025-05-03 02:22:22 -0600

    Create Thread Config

---
+++ thread.config.ts
@@ -0,0 +1,17 @@
+import { createTheme } from 'thread-ui';
+
+const threadConfig = {
+    colors: {
+        primary: {
+            light: '#4f46e5',
+            main: '#4338ca',
+            dark: '#3730a3',
+        },
+        text: {
+            primary: '#4760b3',
+        },
+    },
+} as const;
+
+// Initialize Theme
+export const ThreadTheme = createTheme(threadConfig);
