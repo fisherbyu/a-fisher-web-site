41e998fb7bc78e534ba6e6d58b4a3005b4c19242
Andrew Fisher <fisherandrew777@gmail.com>
2025-05-03 02:22:31 -0600

    Create Providers

---
+++ app/providers.tsx
@@ -0,0 +1,11 @@
+'use client';
+import { ThemeProvider } from 'thread-ui';
+import { ThreadTheme } from '@/thread.config';
+
+export function Providers({ children }: { children: React.ReactNode }) {
+    return (
+        <ThemeProvider initialTheme={ThreadTheme} initialMode="light">
+            {children}
+        </ThemeProvider>
+    );
+}
