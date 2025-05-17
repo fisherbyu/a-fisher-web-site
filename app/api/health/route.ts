3b69b3e56a871a91cf9858822f1e56f20aa1ed57
Andrew Fisher <fisherandrew777@gmail.com>
2025-05-17 13:11:17 -0600

    Make route dynamic

--- app/api/health/route.ts
+++ app/api/health/route.ts
@@ -1,6 +1,8 @@
 import { getArtists } from '@/lib';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: Request) {
     try {
         // Fetch Data, select random
@@ -8,14 +10,31 @@ export async function GET(request: Request) {
         const randomIndex = Math.floor(Math.random() * data.length);
         const randomArtist = data[randomIndex];
 
+        const formattedDate = new Intl.DateTimeFormat('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            timeZoneName: 'short',
+        }).format(new Date());
+
         // Return success
         return NextResponse.json(
             {
                 status: 'success',
                 message: 'API is working properly',
+                timestamp: formattedDate,
                 data: randomArtist,
             },
-            { status: 200 }
+            {
+                status: 200,
+                headers: {
+                    'Cache-Control': 'no-store, max-age=0',
+                    Pragma: 'no-cache',
+                },
+            }
         );
     } catch (error) {
         // Handle errors
