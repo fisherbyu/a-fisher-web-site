ccb284724b6c4a495677b8d275d3afaa1ca61ced
Andrew Fisher <fisherandrew777@gmail.com>
2025-06-07 01:55:09 -0600

    Implement useSWR with temp fetcher

--- app/(main)/music/artists/contents.tsx
+++ app/(main)/music/artists/contents.tsx
@@ -1,14 +1,19 @@
 'use client';
 import MusicDisplay from '@/components/old/ui/music-display';
 import { getArtists } from '@/lib/';
+import { Artist } from '@/types';
+import useSWR from 'swr';
 
+const fetcher = async (url: string): Promise<Artist[]> => {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch artists');
+    }
+
+    return res.json();
+};
 export default async function ArtistContents() {
-    const artists = await getArtists();
-    return (
-        <div>
-            {artists.map((artist) => (
-                <MusicDisplay key={artist.id} data={artist} type="artist" />
-            ))}
-        </div>
-    );
+    const { data: artists, error } = useSWR('/api/artists', fetcher);
+    return <div>{artists?.map((artist) => <MusicDisplay key={artist.id} data={artist} type="artist" />)}</div>;
 }
