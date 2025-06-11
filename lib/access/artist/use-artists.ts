479391cf297642f078b0c3e21916876e517a1fe7
Andrew Fisher <fisherandrew777@gmail.com>
2025-06-10 22:32:28 -0600

    Update useArtists

--- lib/access/artist/use-artists.ts
+++ lib/access/artist/use-artists.ts
@@ -2,16 +2,16 @@ import { makeRequest } from '@/lib/http';
 import { Artist } from '@/types';
 import useSWR from 'swr';
 
-export const fetchArtists = async (): Promise<Artist[]> => {
+const fetchArtists = async (): Promise<Artist[]> => {
     return await makeRequest<Artist[]>('/artist');
 };
 
 export const useArtists = () => {
-    const { data, error, isLoading } = useSWR(`/artist`, fetchArtists);
-    console.log('data', data);
+    const { data, error, isLoading } = useSWR('/artist', fetchArtists);
+
     return {
         artists: data,
         isLoading,
-        error: error,
+        error,
     };
 };
