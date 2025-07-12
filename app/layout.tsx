dacbf0accf6368314b068fffe30a6dedad26e595
Andrew Fisher <fisherandrew777@gmail.com>
2025-07-11 21:37:50 -0600

    Remove providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,9 @@
 import '@/public/styles/globals.css';
 import type { Metadata } from 'next';
 import { Merriweather_Sans } from 'next/font/google';
-import { Providers } from './providers';
-import { ThreadTheme } from 'thread-ui';
 
 // Configure Site
 const MAIN_FONT = Merriweather_Sans({ subsets: ['latin'] });
-const background = ThreadTheme.background;
 
 export let metadata: Metadata = {
     title: 'Andrew Fisher',
@@ -20,9 +17,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" style={MAIN_FONT.style}>
-            <Providers>
-                <body style={{ backgroundColor: background }}>{children}</body>
-            </Providers>
+            <body>{children}</body>
         </html>
     );
 }
