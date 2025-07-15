5cd138e8874622bce1ed64074aed67127dfcef06
Andrew Fisher <fisherandrew777@gmail.com>
2025-07-15 02:01:50 -0600

    Update to use new client exports

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { ThemeProvider } from 'thread-ui';
+import { ThemeProvider } from 'thread-ui/client';
 import { ThreadTheme } from '@/thread.config';
 
 export function Providers({ children }: { children: React.ReactNode }) {
