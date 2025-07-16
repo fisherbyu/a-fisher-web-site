afca669c6d888e5ae8f99837225db9f52202b39e
Andrew Fisher <fisherandrew777@gmail.com>
2025-07-15 22:10:27 -0600

    Simplify with Thread-UI

--- app/(main)/music/artists/page.tsx
+++ app/(main)/music/artists/page.tsx
@@ -1,23 +1,12 @@
 import type { Metadata } from 'next';
-import MusicDisplay from '@/components/old/ui/music-display';
-import MusicDisplayPage from '@/components/old/ui/music/display-music-page';
-import PageTitle from '@/components/old/ui/page-title';
 
-import { useEffect, useState } from 'react';
-import { getData, DataType } from '@/api/dataAccess';
-import { Imprima } from 'next/font/google';
 import ArtistContents from './contents';
-
-const title: { title: string; subtitle?: string; center?: boolean } = {
-    title: 'My Favorite Artists',
-    subtitle: 'Check out some of my Favorite Artists',
-    center: true,
-};
+import { PageHeader } from 'thread-ui';
 
 export default function ArtistPage() {
     return (
         <main>
-            <PageTitle components={title} />
+            <PageHeader title="My Favorite Artists" caption="Check out some of my Favorite Artists" center />
             <ArtistContents />
         </main>
     );
