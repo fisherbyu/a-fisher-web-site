23562b95803951aa6cdb8864d544ded8a66e4a74
Andrew Fisher <fisherandrew777@gmail.com>
2025-07-15 22:05:48 -0600

    Simplify with thread-ui

--- app/(main)/food/page.tsx
+++ app/(main)/food/page.tsx
@@ -1,18 +1,16 @@
 import type { Metadata } from 'next';
-import { InfoCard, InfoCardProps } from 'thread-ui';
-import NotionPreview from '@/components/old/ui/notion-preview';
+import { InfoCard, InfoCardProps, PageHeader } from 'thread-ui';
 import RecipieData from './recipes.json';
-import PageTitle from '@/components/old/ui/page-title';
-const title: { title: string; subtitle?: string } = {
-    title: 'My Recipes',
-    subtitle:
-        "I've loved cooking and baking for as long as I remember.    Its a way for me to express love to others while fulfilling their basic needs and a creative outlet that never runs dry.    Here are some of my favorite recipes.",
-};
+
 export default function FoodPage() {
     const recipes = RecipieData as InfoCardProps[];
     return (
         <main>
-            <PageTitle components={title} />
+            <PageHeader
+                title="My Recipes"
+                caption="I've loved cooking and baking for as long as I remember.    Its a way for me to express love to others while fulfilling their basic needs and a creative outlet that never runs dry.    Here are some of my favorite recipes."
+                center
+            />
             <section className="grid grid-cols-1 gap-1 mt-8 md:grid-cols-2 lg:grid-cols-3 ">
                 {recipes
                     .sort((a, b) => a.title.localeCompare(b.title))
