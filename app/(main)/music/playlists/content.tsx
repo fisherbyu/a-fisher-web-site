c79adac5e9c6abb3a7e9900bd4e669c393028bf8
Andrew Fisher <fisherandrew777@gmail.com>
2025-07-15 22:41:39 -0600

    Simplify with Thread-UI

---
+++ app/(main)/music/playlists/content.tsx
@@ -0,0 +1,119 @@
+'use client';
+import Image from 'next/image';
+import { useState } from 'react';
+import AppleMusicLogo from '@/public/music/apple-music.svg';
+import SpotifyLogo from '@/public/music/spotify.svg';
+import { ColumnItem, ColumnLayout } from 'thread-ui';
+
+interface Playlist {
+    title: string;
+    appleURI: string;
+    spotifyURI: string;
+}
+
+const playlists: Playlist[] = [
+    {
+        title: 'Autumn Yellow',
+        appleURI: 'autumn-yellow/pl.u-NpXmEkVC4YpKoW',
+        spotifyURI: '3t7Mg9EtMDpu7730lsvolB',
+    },
+    {
+        title: 'Cooking Up Bops',
+        appleURI: 'cooking-up-bops/pl.u-8aAVp8qCvla2z5',
+        spotifyURI: '3Lr4DjCEJETTCXBw93hZgm',
+    },
+    {
+        title: 'Spring Violet',
+        appleURI: 'spring-violet/pl.u-jV892oDCDGqLxJ',
+        spotifyURI: '3xSOnUvh7ixFG7pg8toTJy',
+    },
+    {
+        title: 'Summer Pop',
+        appleURI: 'summer-pop/pl.u-kv9l1ZVsJzWZrE',
+        spotifyURI: '6Qbl1IHrmDJJzagNdFfMV3',
+    },
+];
+
+export const PlaylistContents = () => {
+    // iFrame Constuction Logic
+    const getAppleFrame = (uri: string) => {
+        const link = `https://embed.music.apple.com/us/playlist/${uri}`;
+        return (
+            <iframe
+                allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
+                width="100%"
+                height="450"
+                sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation fullscreen picture-in-picture"
+                src={link}
+            ></iframe>
+        );
+    };
+
+    const getSpotifyFrame = (uri: string) => {
+        const link = `https://open.spotify.com/embed/playlist/${uri}?utm_source=generator`;
+        return (
+            <iframe
+                src={link}
+                width="100%"
+                height="352"
+                frameBorder="0"
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                loading="lazy"
+            ></iframe>
+        );
+    };
+
+    // Determine which to Display
+    const [displayType, setDisplayType] = useState<'Apple' | 'Spotify'>('Apple');
+
+    function switchToApple() {
+        setDisplayType('Apple');
+    }
+
+    function switchToSpotify() {
+        setDisplayType('Spotify');
+    }
+
+    const displayFrame = (playlist: Playlist) => {
+        if (displayType == 'Apple') {
+            return getAppleFrame(playlist.appleURI);
+        } else {
+            return getSpotifyFrame(playlist.spotifyURI);
+        }
+    };
+
+    const applePlaylists: ColumnItem[] = playlists.map((playlist) => ({
+        content: (
+            <div key={playlist.title} className="w-full md:w-11/12 lg:w-full mx-auto">
+                {getAppleFrame(playlist.appleURI)}
+            </div>
+        ),
+    }));
+
+    const spotifyPlaylists: ColumnItem[] = playlists.map((playlist) => ({
+        content: (
+            <div key={playlist.title} className="w-full md:w-11/12 lg:w-full mx-auto">
+                {getSpotifyFrame(playlist.spotifyURI)}
+            </div>
+        ),
+    }));
+
+    return (
+        <div className="container">
+            <div className="mx-auto flex flex-row items-center justify-center gap-2 pb-4">
+                <button className="" onClick={switchToApple}>
+                    <Image className=" w-9" src={AppleMusicLogo} alt="Logo, Apple Music" />
+                </button>
+                <button className="" onClick={switchToSpotify}>
+                    <Image className=" w-9" src={SpotifyLogo} alt="Logo, Spotify" />
+                </button>
+            </div>
+            <div id="applemusic" className={displayType === 'Apple' ? '' : 'hidden'}>
+                <ColumnLayout mdcol={1} lgcol={2} items={applePlaylists} />
+            </div>
+            <div id="spotify" className={displayType === 'Spotify' ? '' : 'hidden'}>
+                <ColumnLayout mdcol={1} lgcol={2} items={spotifyPlaylists} />
+            </div>
+        </div>
+    );
+};
--- app/(main)/music/playlists/page.tsx
+++ app/(main)/music/playlists/page.tsx
@@ -1,41 +1,13 @@
 import type { Metadata } from 'next';
 import DisplayPlaylists from '@/components/old/ui/display-playlists';
 import { PageHeader } from 'thread-ui';
-
-interface Playlist {
-    title: string;
-    appleURI: string;
-    spotifyURI: string;
-}
-
-const playlists: Playlist[] = [
-    {
-        title: 'Autumn Yellow',
-        appleURI: 'autumn-yellow/pl.u-NpXmEkVC4YpKoW',
-        spotifyURI: '3t7Mg9EtMDpu7730lsvolB',
-    },
-    {
-        title: 'Cooking Up Bops',
-        appleURI: 'cooking-up-bops/pl.u-8aAVp8qCvla2z5',
-        spotifyURI: '3Lr4DjCEJETTCXBw93hZgm',
-    },
-    {
-        title: 'Spring Violet',
-        appleURI: 'spring-violet/pl.u-jV892oDCDGqLxJ',
-        spotifyURI: '3xSOnUvh7ixFG7pg8toTJy',
-    },
-    {
-        title: 'Summer Pop',
-        appleURI: 'summer-pop/pl.u-kv9l1ZVsJzWZrE',
-        spotifyURI: '6Qbl1IHrmDJJzagNdFfMV3',
-    },
-];
+import { PlaylistContents } from './content';
 
 export default function PlaylistPage() {
     return (
         <main>
             <PageHeader title="My Playlists" caption="Here are some of my favorite playlists" center />
-            <DisplayPlaylists components={playlists} />
+            <PlaylistContents />
         </main>
     );
 }
