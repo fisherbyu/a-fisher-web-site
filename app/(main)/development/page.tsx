2d49000da18a4388f3a49eafc7b1c6b96a64b931
Andrew Fisher <fisherandrew777@gmail.com>
2025-07-15 22:03:02 -0600

    Use Thread Components

--- app/(main)/development/page.tsx
+++ app/(main)/development/page.tsx
@@ -1,24 +1,21 @@
 import type { Metadata } from 'next';
 import ArticleData from './articles.json';
-import PageTitle from '@/components/old/ui/page-title';
-import { InfoCard, InfoCardProps } from 'thread-ui';
-
-const title: { title: string; subtitle?: string } = {
-    title: 'My Software Projects',
-    subtitle:
-        "Here is where I'll display projects I've worked on to display some of the things I've learned or taught myself.    I've also written a few articles on different technologies to serve as guides/quick references.",
-};
+import { InfoCard, InfoCardProps, PageHeader } from 'thread-ui';
 
 export default function DevelopmentPage() {
     const articles = ArticleData as InfoCardProps[];
     return (
         <main>
-            <PageTitle components={title} />
+            <PageHeader
+                title="My Software Projects"
+                caption="Here is where I'll display projects I've worked on to display some of the things I've learned or taught myself.    I've also written a few articles on different technologies to serve as guides/quick references."
+                center
+            />
             <section className="container">
                 <h1 className="text-left dark:text-white text-2xl md:text-3xl xl:text-4xl font-medium text-gray-900 w-10/12 p-5">
                     Articles
                 </h1>
-                <div className="grid grid-cols-1 gap-12 mt-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-8 ">
+                <div className="grid grid-cols-1 gap-12 mt-4 md:grid-cols-2 lg:grid-cols-3 lg:gap-8 ">
                     {articles.map((article, index) => (
                         <InfoCard key={index} title={article.title} url={article.url} icon={article.icon} img={article.img} />
                     ))}
