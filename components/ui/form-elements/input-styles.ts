a70a84604d3599e0587408a517e1c38f12572db6
Andrew Fisher <fisherandrew777@gmail.com>
2025-07-21 16:36:47 -0600

    Fix styles

--- components/ui/form-elements/input-styles.ts
+++ components/ui/form-elements/input-styles.ts
@@ -3,7 +3,6 @@ const BASE_STYLES = {
     layout: 'px-4 py-2',
     border: 'border border-gray-300 rounded-md',
     focus: 'focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-colors',
-    darkBg: '',
 };
 
 export const INPUT_STYLES = {
