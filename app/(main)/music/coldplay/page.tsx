3d95fa25c467308640063b73e023eee64e684a88
Andrew Fisher <fisherandrew777@gmail.com>
2025-07-21 21:51:36 -0600

    Updates

--- app/(main)/music/coldplay/page.tsx
+++ app/(main)/music/coldplay/page.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 // Import Content
 import content from '@/api/static_content/albums.json';
 import { PageHeader } from '@/.yalc/thread-ui/dist';
+import { MusicCard } from '@/components/features/music-card/music-card';
 
 export default function ColdplayPage() {
     return (
@@ -10,8 +11,8 @@ export default function ColdplayPage() {
             <PageHeader title="Coldplay Albums" caption="My review of each Coldplay Album" center />
             <section className="container">
                 {content.map((album, index) => (
-                    // <MusicDisplay key={album.id} data={album} type="album" />
-                    <div key={index}></div>
+                    // <MusicCard key={index} item={album} type="album" />
+                    <></>
                 ))}
                 <br />
             </section>
