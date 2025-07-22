c8bbcc4c96324435785d023ea34eb123d4cb9e23
Andrew Fisher <fisherandrew777@gmail.com>
2025-07-22 00:18:41 -0600

    Create createAlbum action

---
+++ lib/actions/data/albums/create-album.ts
@@ -0,0 +1,60 @@
+'use server';
+import { Album, AlbumDto } from '@/types';
+import { prisma, transformAlbum } from '@/lib';
+
+/**
+ * Server Action to Create Album
+ * @param {AlbumDto} data
+ * @returns {Promise<Album>}
+ */
+export async function createArtist(data: AlbumDto): Promise<Album> {
+    // Extract Data
+    const {
+        name,
+        rank,
+        link: { id: linkId, ...linkData },
+        image: { id: imgId, ...imgData },
+        contents: contentsWithIds,
+        attributes: attributesWithIds,
+    } = data;
+
+    // Remove IDs from arrays
+    const contents = contentsWithIds.map(({ id, ...rest }) => rest);
+    const attributes = attributesWithIds.map(({ id, ...rest }) => rest);
+
+    const album = await prisma.album.create({
+        data: {
+            name,
+            rank,
+            link: {
+                create: linkData,
+            },
+            image: {
+                create: imgData,
+            },
+            contents: {
+                create: contents,
+            },
+            attributes: {
+                create: attributes,
+            },
+        },
+        // Return full Album Object
+        include: {
+            link: true,
+            image: true,
+            contents: {
+                orderBy: {
+                    order: 'asc',
+                },
+            },
+            attributes: {
+                orderBy: {
+                    order: 'asc',
+                },
+            },
+        },
+    });
+
+    return transformAlbum(album);
+}
