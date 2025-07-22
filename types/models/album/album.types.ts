7f1f5d4d4ccf5370f9fc9f5d8286b9da87b51e9d
Andrew Fisher <fisherandrew777@gmail.com>
2025-07-22 00:31:43 -0600

    Make rank optional

--- types/models/album/album.types.ts
+++ types/models/album/album.types.ts
@@ -4,7 +4,7 @@ import { DtoId } from '../dto';
 export type Album = {
     id: number;
     name: string;
-    rank: number;
+    rank?: number;
     contents: Content[];
     attributes: Attribute[];
     link?: Link;
