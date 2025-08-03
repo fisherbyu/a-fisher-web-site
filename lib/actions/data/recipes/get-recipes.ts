8189fb6ba349b71dfb76b0b81392a024cab199d7
Andrew Fisher <fisherandrew777@gmail.com>
2025-08-03 17:19:20 -0600

    Access recipes

--- lib/actions/data/recipes/get-recipes.ts
+++ lib/actions/data/recipes/get-recipes.ts
@@ -1,3 +1,28 @@
-export const getRecipes = () => {
-    return 'recipes';
+'use server';
+import { notion } from '@/lib/notion';
+import { transformRecipe } from '@/lib/utils';
+import { ApiResponse } from '@/types';
+import { Recipe } from '@/types/models/recipe';
+
+const RECIPE_DB_ID = '9d17ef1c79914432896a680416f9ba1b';
+
+export const getRecipes = async () => {
+    const recipeData = await notion.databases.query({
+        database_id: RECIPE_DB_ID,
+    });
+
+    return recipeData.results.map((recipe) => transformRecipe(recipe));
+};
+
+export const fetchRecipes = async (): Promise<ApiResponse<Recipe[]>> => {
+    try {
+        return {
+            data: await getRecipes(),
+        };
+    } catch (error) {
+        return {
+            error: 'Failed to fetch Recipes from Notion',
+            message: error instanceof Error ? error.message : 'Unknown error fetching recipes',
+        };
+    }
 };
