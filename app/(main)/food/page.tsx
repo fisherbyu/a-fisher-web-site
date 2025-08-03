4ecfa39b6a9745b7f6ed8ad094d572657ff5d99a
Andrew Fisher <fisherandrew777@gmail.com>
2025-08-03 17:32:36 -0600

    Make Recipe Data Dynamic

--- app/(main)/food/page.tsx
+++ app/(main)/food/page.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from 'next';
-import { InfoCard, InfoCardProps, PageHeader } from 'thread-ui';
-import RecipieData from './recipes.json';
+import { PageHeader } from 'thread-ui';
+import RecipeContents from './contents';
 
 export default function FoodPage() {
-    const recipes = RecipieData as InfoCardProps[];
     return (
         <>
             <PageHeader
@@ -11,13 +10,7 @@ export default function FoodPage() {
                 caption="I've loved cooking and baking for as long as I remember.    Its a way for me to express love to others while fulfilling their basic needs and a creative outlet that never runs dry.    Here are some of my favorite recipes."
                 center
             />
-            <section className="grid grid-cols-1 gap-1 mt-8 md:grid-cols-2 lg:grid-cols-3 ">
-                {recipes
-                    .sort((a, b) => a.title.localeCompare(b.title))
-                    .map((recipe, index) => (
-                        <InfoCard key={index} title={recipe.title} url={recipe.url} icon={recipe.icon} img={recipe.img} />
-                    ))}
-            </section>
+            <RecipeContents />
         </>
     );
 }
