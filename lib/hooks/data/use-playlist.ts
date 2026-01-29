2377ae7d5d638a568674f98ae70ace886d9ce7b6
Andrew Fisher <fisherandrew777@gmail.com>
2026-01-28 22:04:16 -0700

    Utilize new fetch function

--- lib/hooks/data/use-playlist.ts
+++ lib/hooks/data/use-playlist.ts
@@ -1,9 +1,9 @@
 'use client';
-import { fetchPlaylist } from '@/lib';
+import { fetchPlaylists } from '@/lib';
 import useSWR from 'swr';
 
 export const usePlaylists = () => {
-    const { data, error, isLoading } = useSWR('/playlist', fetchPlaylist);
+    const { data, error, isLoading } = useSWR('/playlist', fetchPlaylists);
     return {
         playlists: data?.data,
         isLoading,
