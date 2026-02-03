9e2499868d225bb280032485a9ea3db57aba5f0b
Andrew Fisher <fisherandrew777@gmail.com>
2026-02-02 23:21:35 -0700

    Ensure Image is included with album

--- types/models/album/album.types.ts
+++ types/models/album/album.types.ts
@@ -1,4 +1,13 @@
-import { Attribute, AttributeDto, Content, ContentDto, Image, ImageDto, Link, LinkDto } from '@/types';
+import {
+    Attribute,
+    AttributeDto,
+    Content,
+    ContentDto,
+    Image,
+    ImageDto,
+    Link,
+    LinkDto,
+} from '@/types';
 import { DtoId } from '../dto';
 
 export type Album = {
@@ -8,7 +17,7 @@ export type Album = {
     contents: Content[];
     attributes: Attribute[];
     link?: Link;
-    image?: Image;
+    image: Image;
 };
 
 export type AlbumDto = Omit<Album, 'id' | 'contents' | 'attributes' | 'link' | 'image'> & {
