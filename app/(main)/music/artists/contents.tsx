92702230ca1cd54cb41cf25abe2ebdbfb3c96d21
Andrew Fisher <fisherandrew777@gmail.com>
2026-02-02 23:50:04 -0700

    Upgrade to thread-ui

--- app/(main)/music/artists/contents.tsx
+++ app/(main)/music/artists/contents.tsx
@@ -1,8 +1,46 @@
 'use client';
-import { MusicCard } from '@/components';
-import { useArtists } from '@/lib';
+import { MediaCard } from 'thread-ui';
+import { getMusicLink, getPublicUrl, useArtists } from '@/lib';
+import Image from 'next/image';
+import Link from 'next/link';
+import AppleMusicLogo from '@/public/music/apple-music.svg';
+import SpotifyLogo from '@/public/music/spotify.svg';
 
 export default function ArtistContents() {
     const { artists, isLoading, error } = useArtists();
-    return <div>{artists?.map((artist, _) => <MusicCard key={_} index={_} item={artist} type="artist" />)}</div>;
+
+    return (
+        <div className="flex flex-col gap-4">
+            {artists?.map((artist, _) => (
+                <MediaCard
+                    key={_}
+                    title={artist.name}
+                    description={artist.contents.map((content) => content.text)}
+                    details={artist.attributes.map((attribute) => ({
+                        title: attribute.title,
+                        details: attribute.text,
+                    }))}
+                    detailsPosition="text"
+                    image={
+                        <Image
+                            src={getPublicUrl(artist.image.src)}
+                            alt={artist.image.alt}
+                            height={artist.image.height}
+                            width={artist.image.width}
+                        />
+                    }
+                    imagePosition={_ % 2 === 0 ? 'right' : 'left'}
+                    size="md"
+                    links={[
+                        <Link href={getMusicLink(artist.link.appleURI, 'artist', 'apple')}>
+                            <Image className=" w-5" src={AppleMusicLogo} alt="Logo, Apple Music" />
+                        </Link>,
+                        <Link href={getMusicLink(artist.link.spotifyURI, 'artist', 'spotify')}>
+                            <Image className=" w-5" src={SpotifyLogo} alt="Logo, Spotify" />
+                        </Link>,
+                    ]}
+                />
+            ))}
+        </div>
+    );
 }
--- app/(main)/music/coldplay/contents.tsx
+++ app/(main)/music/coldplay/contents.tsx
@@ -1,8 +1,46 @@
 'use client';
-import { MusicCard } from '@/components';
-import { useAlbums } from '@/lib';
+import { MediaCard } from 'thread-ui';
+import { getMusicLink, getPublicUrl, useAlbums } from '@/lib';
+import Image from 'next/image';
+import Link from 'next/link';
+import AppleMusicLogo from '@/public/music/apple-music.svg';
+import SpotifyLogo from '@/public/music/spotify.svg';
 
 export default function AlbumContents() {
     const { albums, isLoading, error } = useAlbums();
-    return <div>{albums?.map((album, _) => <MusicCard key={_} index={_} item={album} type="album" />)}</div>;
+
+    return (
+        <div className="flex flex-col gap-4">
+            {albums?.map((album, _) => (
+                <MediaCard
+                    key={_}
+                    title={album.name}
+                    description={album.contents.map((content) => content.text)}
+                    details={album.attributes.map((attribute) => ({
+                        title: attribute.title,
+                        details: attribute.text,
+                    }))}
+                    detailsPosition="image"
+                    image={
+                        <Image
+                            src={getPublicUrl(album.image.src)}
+                            alt={album.image.alt}
+                            height={album.image.height}
+                            width={album.image.width}
+                        />
+                    }
+                    imagePosition={_ % 2 === 0 ? 'right' : 'left'}
+                    size="lg"
+                    links={[
+                        <Link href={getMusicLink(album.link.appleURI, 'album', 'apple')}>
+                            <Image className=" w-5" src={AppleMusicLogo} alt="Logo, Apple Music" />
+                        </Link>,
+                        <Link href={getMusicLink(album.link.spotifyURI, 'album', 'spotify')}>
+                            <Image className=" w-5" src={SpotifyLogo} alt="Logo, Spotify" />
+                        </Link>,
+                    ]}
+                />
+            ))}
+        </div>
+    );
 }
