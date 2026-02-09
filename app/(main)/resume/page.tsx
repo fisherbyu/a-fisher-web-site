9cfea8b7eb0213c92c4c52ee4b80e3216c4f6551
Andrew Fisher <fisherandrew777@gmail.com>
2026-02-08 22:43:51 -0700

    Implement thread ui in resume

--- app/(main)/resume/page.tsx
+++ app/(main)/resume/page.tsx
@@ -3,7 +3,7 @@ import BYULogo from '@/public/resume/byu-logo.png';
 import Image from 'next/image';
 import resume from './resume.json';
 import { ReactNode } from 'react';
-import { Divider, PageHeader, H2 } from 'thread-ui';
+import { Divider, PageHeader, H2, List, OrderedList, Card, H3, Text, H1 } from 'thread-ui';
 
 type CardProps = {
     size?: 'sm' | 'md' | 'lg';
@@ -11,19 +11,32 @@ type CardProps = {
     children: ReactNode;
 };
 
-const Card = ({ size = 'md', title, children }: CardProps) => {
-    return (
-        <div className="p-5 rounded-lg border bg-card text-card-foreground shadow-sm w-full md:w-9/12 mx-auto max-w-[850px] mb-5">
-            {title && (
-                <div>
-                    <h1 className="text-xl font-semibold leading-none tracking-tight">{title}</h1>
-                    <Divider />
-                </div>
-            )}
-            {children}
-        </div>
-    );
-};
+// const Card = ({ size = 'md', title, children }: CardProps) => {
+//     const feature = true;
+
+//     if (feature) {
+//         return (
+//             <ThreadCard
+//                 surfaceColor="surface"
+//                 title={{ text: title || '', divider: true, align: 'left' }}
+//                 size="md"
+//             >
+//                 {children}
+//             </ThreadCard>
+//         );
+//     }
+//     return (
+//         <div className="p-5 rounded-lg border bg-card text-card-foreground shadow-sm w-full md:w-9/12 mx-auto max-w-[850px] mb-5">
+//             {title && (
+//                 <div>
+//                     <h1 className="text-xl font-semibold leading-none tracking-tight">{title}</h1>
+//                     <Divider />
+//                 </div>
+//             )}
+//             {children}
+//         </div>
+//     );
+// };
 
 export default function ResumePage() {
     const LAYOUT_WIDTH = 'w-full md:w-9/12 mx-auto max-w-[850px]';
@@ -32,63 +45,58 @@ export default function ResumePage() {
             <PageHeader title="Resume" />
             <div className="container mb-6">
                 <div className={LAYOUT_WIDTH}>
-                    <H2>Education</H2>
+                    <H1>Education</H1>
                 </div>
-                <div className="p-5 rounded-lg border bg-card text-card-foreground shadow-sm w-full md:w-9/12 mx-auto max-w-[850px]">
+                <Card>
                     <div className="flex justify-start items-center space-x-2 pb-4">
                         <Image src={BYULogo} alt="BYU Logo" className=" w-10" />
                         <div>
-                            <h1 className="text-xl font-semibold leading-none tracking-tight">{resume.education[0].institution}</h1>
-                            <p className="text-sm text-muted-foreground">
-                                {resume.education[0].graduation_month} {resume.education[0].graduation_year} -{' '}
-                                {resume.education[0].city}, {resume.education[0].state}
-                            </p>
+                            <H2 inline>{resume.education[0].institution}</H2>
+                            <Text size="xs">
+                                {resume.education[0].graduation_month}{' '}
+                                {resume.education[0].graduation_year} - {resume.education[0].city},{' '}
+                                {resume.education[0].state}
+                            </Text>
                         </div>
                     </div>
-                    <h2 className=" text-lg font-normal leading-none tracking-tight">{resume.education[0].degree}</h2>
-                    <p className="text-sm text-muted-foreground">{resume.education[0].college}</p>
-                    <p className="pb-1"></p>
-                    <p className=" text-xs">{resume.education[0].additionals}</p>
-                </div>
+                    <H3 inline>{resume.education[0].degree}</H3>
+                    <Text color="text-secondary" bold size="md">
+                        {resume.education[0].college}
+                    </Text>
+                    <br />
+                    <Text size="sm">{resume.education[0].additionals}</Text>
+                </Card>
             </div>
-            <div className="container mb-6">
+            <div className="container mb-6 flex flex-col gap-4">
                 <div className={LAYOUT_WIDTH}>
-                    <H2>Experience</H2>
+                    <H1>Experience</H1>
                 </div>
                 {resume.jobs.map((job) => (
-                    <Card title={job.title} key={job.title}>
-                        <p className=" font-normal text-base">{job.company}</p>
+                    <Card title={{ text: job.title, divider: true }} key={job.title}>
+                        <Text bold>{job.company}</Text>
                         <span className="flex justify-between items-center">
-                            <p className="text-sm text-muted-foreground">
-                                {job.start_date} - {job.end_date !== null ? job.end_date : 'Present'}
-                            </p>
-                            <p className="text-sm text-muted-foreground">
-                                {job.city}, {job.state} {job.country !== 'USA' ? `- ${job.country}` : ''}
-                            </p>
+                            <Text size="sm">
+                                {job.start_date} -{' '}
+                                {job.end_date !== null ? job.end_date : 'Present'}
+                            </Text>
+                            <Text size="sm">
+                                {job.city}, {job.state}{' '}
+                                {job.country !== 'USA' ? `- ${job.country}` : ''}
+                            </Text>
                         </span>
-                        <ul className="list-disc pl-5">
-                            {job.descriptions.map((desc, index) => (
-                                <li key={index} className=" font-light">
-                                    {desc}
-                                </li>
-                            ))}
-                        </ul>
+                        <div className="mt-2">
+                            <List size="sm" items={job.descriptions} />
+                        </div>
                     </Card>
                 ))}
             </div>
             <div className="container mb-6">
                 <div className={LAYOUT_WIDTH}>
-                    <H2>Skills & Achievements</H2>
+                    <H1>Skills & Achievements</H1>
                 </div>
-                <div className="p-5 rounded-lg border bg-card text-card-foreground shadow-sm w-full md:w-9/12 mb-5 mx-auto max-w-[850px]">
-                    <ul className="list-disc pl-5">
-                        {resume.skills.map((skill, index) => (
-                            <li key={index} className=" font-light">
-                                {skill}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
+                <Card>
+                    <List items={resume.skills} />
+                </Card>
             </div>
         </>
     );
