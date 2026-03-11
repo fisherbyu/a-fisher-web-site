f4a04c30d460e6423c86a3185e166ab665a6faa2
Andrew Fisher <fisherandrew777@gmail.com>
2026-03-10 18:14:19 -0600

    Add sorting to food display

--- app/(main)/food/contents.tsx
+++ app/(main)/food/contents.tsx
@@ -1,35 +1,54 @@
 'use client';
-import { ColumnLayout, InfoCard, SkeletonLayout } from 'thread-ui';
+import { ColumnLayout, InfoCard, SkeletonLayout, SortControls } from 'thread-ui';
 import { useRecipes } from '@/lib';
+import { useSortControls } from 'thread-ui';
 
 export default function RecipeContents() {
     const { recipes, isLoading, error } = useRecipes();
 
+    const { sortedData, sortControlsProps } = useSortControls({
+        data: recipes || [],
+        fields: [
+            {
+                key: 'type',
+                label: 'Type',
+                sortOrder: ['Main', 'Side', 'Dessert', 'Bread', 'Sauce', 'Condiment'],
+            },
+            { key: 'title', label: 'Title' },
+        ],
+        multi: true,
+        defaultSort: [
+            { key: 'type', direction: 'asc' },
+            { key: 'title', direction: 'asc' },
+        ],
+    });
+
     if (isLoading) {
         return (
-            <SkeletonLayout mdcol={2} lgcol={3} rows={1} itemConfig={{ h: '238px', w: '391px' }} />
+            <SkeletonLayout mdcol={2} lgcol={3} rows={3} itemConfig={{ h: '238px', w: '391px' }} />
         );
     }
 
     return (
         <>
+            <div className="max-w-[1400px] px-16 mx-auto">
+                <SortControls color="secondary" {...sortControlsProps} />
+            </div>
             <ColumnLayout
                 mdcol={2}
                 lgcol={3}
                 items={
-                    recipes
-                        ?.sort((a, b) => a.title.localeCompare(b.title))
-                        .map((recipe, _) => ({
-                            content: (
-                                <InfoCard
-                                    key={_}
-                                    title={recipe.title}
-                                    url={recipe.url}
-                                    icon={recipe.icon}
-                                    img={recipe.img}
-                                />
-                            ),
-                        })) ?? []
+                    sortedData?.map((recipe) => ({
+                        key: recipe.url,
+                        content: (
+                            <InfoCard
+                                title={recipe.title}
+                                url={recipe.url}
+                                icon={recipe.icon}
+                                img={recipe.img}
+                            />
+                        ),
+                    })) ?? []
                 }
             />
         </>
