0ac8148089ad9ef4102f2bfc4b7eed11d1a66d6e
Andrew Fisher <fisherandrew777@gmail.com>
2026-03-16 21:46:06 -0600

    Wrap in container

--- app/(main)/music/artists/contents.tsx
+++ app/(main)/music/artists/contents.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { MediaCard, SkeletonLayout } from 'thread-ui';
+import { Container, MediaCard, SkeletonLayout } from 'thread-ui';
 import { getMusicLink, getPublicUrl, useArtists } from '@/lib';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -23,37 +23,43 @@ export default function ArtistContents() {
     }
 
     return (
-        <div className="flex flex-col gap-4">
-            {artists?.map((artist, _) => (
-                <MediaCard
-                    key={_}
-                    title={artist.name}
-                    description={artist.contents.map((content) => content.text)}
-                    details={artist.attributes.map((attribute) => ({
-                        title: attribute.title,
-                        details: attribute.text,
-                    }))}
-                    detailsPosition="text"
-                    image={
-                        <Image
-                            src={getPublicUrl(artist.image.src)}
-                            alt={artist.image.alt}
-                            height={artist.image.height}
-                            width={artist.image.width}
-                        />
-                    }
-                    imagePosition={_ % 2 === 0 ? 'right' : 'left'}
-                    size="md"
-                    links={[
-                        <Link href={getMusicLink(artist.link.appleURI, 'artist', 'apple')}>
-                            <Image className=" w-5" src={AppleMusicLogo} alt="Logo, Apple Music" />
-                        </Link>,
-                        <Link href={getMusicLink(artist.link.spotifyURI, 'artist', 'spotify')}>
-                            <Image className=" w-5" src={SpotifyLogo} alt="Logo, Spotify" />
-                        </Link>,
-                    ]}
-                />
-            ))}
-        </div>
+        <Container>
+            <div className="flex flex-col gap-4">
+                {artists?.map((artist, _) => (
+                    <MediaCard
+                        key={_}
+                        title={artist.name}
+                        description={artist.contents.map((content) => content.text)}
+                        details={artist.attributes.map((attribute) => ({
+                            title: attribute.title,
+                            details: attribute.text,
+                        }))}
+                        detailsPosition="text"
+                        image={
+                            <Image
+                                src={getPublicUrl(artist.image.src)}
+                                alt={artist.image.alt}
+                                height={artist.image.height}
+                                width={artist.image.width}
+                            />
+                        }
+                        imagePosition={_ % 2 === 0 ? 'right' : 'left'}
+                        size="md"
+                        links={[
+                            <Link href={getMusicLink(artist.link.appleURI, 'artist', 'apple')}>
+                                <Image
+                                    className=" w-5"
+                                    src={AppleMusicLogo}
+                                    alt="Logo, Apple Music"
+                                />
+                            </Link>,
+                            <Link href={getMusicLink(artist.link.spotifyURI, 'artist', 'spotify')}>
+                                <Image className=" w-5" src={SpotifyLogo} alt="Logo, Spotify" />
+                            </Link>,
+                        ]}
+                    />
+                ))}
+            </div>
+        </Container>
     );
 }
