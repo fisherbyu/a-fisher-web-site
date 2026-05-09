db31b0873270b5cb94f24607d4800fa7bfd0cd27
Andrew Fisher <fisherandrew777@gmail.com>
2026-05-08 23:07:10 -0600

    Update transformer to utilize new props

--- lib/utils/data-transformers/transform-recipe.ts
+++ lib/utils/data-transformers/transform-recipe.ts
@@ -2,11 +2,12 @@ import { Recipe } from '@/types';
 import { NotionIcon } from 'thread-ui';
 
 export const transformRecipe = (recipe: any): Recipe => {
+    const imageSrc = recipe.cover?.external?.url ?? recipe.cover?.file?.url ?? null;
     return {
         id: recipe.id,
         title: recipe.properties.Title.title[0].text.content,
         url: recipe.url,
-        img: recipe.cover?.external?.url ?? recipe.cover?.file?.url ?? null,
+        image: imageSrc ? { src: imageSrc } : null,
         icon: recipe.icon as NotionIcon | undefined,
         type: recipe.properties.Type?.select?.name ?? null,
         tags: recipe.properties.Tags.multi_select.map((tag: any) => {
