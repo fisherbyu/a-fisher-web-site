9b851fea2356de447aa814818a1735e142043e87
Andrew Fisher <fisherandrew777@gmail.com>
2026-05-19 00:35:11 -0600

    Blur large images

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -10,7 +10,7 @@ import Utah from '@/public/home/alpine-loop.jpg';
 import HBLL from '@/public/home/hbll.jpg';
 import Philippines from '@/public/home/philippines.jpg';
 // Import Components
-import { Divider, ColumnLayout, ImagePanel, Title } from 'thread-ui';
+import { Divider, ColumnLayout, ImagePanel, Title, SkeletonLoader } from 'thread-ui';
 
 // Content
 export default function Home() {
@@ -23,8 +23,22 @@ export default function Home() {
                 contents={[
                     'I love music, history, and cooking for my friends.  I use technology to solve problems for people.  I recently graduated from BYU as a Master of Information Systems Managment.',
                 ]}
-                image={<Image src={ImageAF_3_2} alt="Picture of Andrew Fisher" />}
-                smImage={<Image src={ImageAF_5_4} alt="Picture of Andrew Fisher" />}
+                image={
+                    <Image
+                        placeholder="blur"
+                        loading="eager"
+                        src={ImageAF_3_2}
+                        alt="Picture of Andrew Fisher"
+                    />
+                }
+                smImage={
+                    <Image
+                        placeholder="blur"
+                        loading="eager"
+                        src={ImageAF_5_4}
+                        alt="Picture of Andrew Fisher"
+                    />
+                }
             />
             <Divider width="75%" marginY="36px" />
             {/* About Me Section */}
@@ -40,24 +54,43 @@ export default function Home() {
                         title: 'Folsom',
                         description:
                             'I grew up in Folsom, California.    It was a great place to grow up with tons of friendly people and opportunities for me as a youth',
-                        content: <Image src={Folsom} alt={'Rainbow Bridge, Folsom CA'} />,
+                        content: (
+                            <Image
+                                placeholder="blur"
+                                src={Folsom}
+                                alt={'Rainbow Bridge, Folsom CA'}
+                            />
+                        ),
                     },
                     {
                         title: 'Philippines',
                         description:
                             'I served a mission for The Church of Jesus Christ of Latter-day Saints in Isabela Philippines.    The people of the Philippines are the most friendly, kind and amazing people on Earth.',
-                        content: <Image src={Philippines} alt={'Corn Field, Echague Philippines'} />,
+                        content: (
+                            <Image
+                                placeholder="blur"
+                                src={Philippines}
+                                alt={'Corn Field, Echague Philippines'}
+                            />
+                        ),
                     },
                     {
                         title: 'BYU',
                         description:
                             'I recently graduated from BYU.    I loved learning in such a spiritual environment and I made lifelong friends during my time here.',
-                        content: <Image src={HBLL} alt="HBLL Library at BYU" />,
+                        content: <Image placeholder="blur" src={HBLL} alt="HBLL Library at BYU" />,
                     },
                     {
                         title: 'Utah',
-                        description: "I'm lucky to live in Utah, it's such a pretty place with so much to do.",
-                        content: <Image src={Utah} alt="Autumn leaves in Alpine Loop, Provo Canyon" />,
+                        description:
+                            "I'm lucky to live in Utah, it's such a pretty place with so much to do.",
+                        content: (
+                            <Image
+                                placeholder="blur"
+                                src={Utah}
+                                alt="Autumn leaves in Alpine Loop, Provo Canyon"
+                            />
+                        ),
                     },
                 ]}
             />
@@ -69,13 +102,31 @@ export default function Home() {
                 lgcol={3}
                 items={[
                     {
-                        content: <Image src={LaLaLand} alt="La La Land Movie Poster" />,
+                        content: (
+                            <Image
+                                placeholder="blur"
+                                src={LaLaLand}
+                                alt="La La Land Movie Poster"
+                            />
+                        ),
                     },
                     {
-                        content: <Image src={WalterMitty} alt="The Secret Life of Walter Mitty Movie Poster" />,
+                        content: (
+                            <Image
+                                placeholder="blur"
+                                src={WalterMitty}
+                                alt="The Secret Life of Walter Mitty Movie Poster"
+                            />
+                        ),
                     },
                     {
-                        content: <Image src={SpiderMan} alt="Spider Man: Into the Spiderverse Movie Poster" />,
+                        content: (
+                            <Image
+                                placeholder="blur"
+                                src={SpiderMan}
+                                alt="Spider Man: Into the Spiderverse Movie Poster"
+                            />
+                        ),
                     },
                 ]}
             />
