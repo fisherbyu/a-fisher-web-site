95dbbce93baf33d89918fdfb1c57e3fa89911651
Andrew Fisher <fisherandrew777@gmail.com>
2026-06-14 23:19:53 -0600

    Apply format, set type

--- lib/supabase/update-session.ts
+++ lib/supabase/update-session.ts
@@ -10,20 +10,34 @@ export async function updateSession(request: NextRequest) {
         request,
     });
 
-    const supabase = createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
-        cookies: {
-            getAll() {
-                return request.cookies.getAll();
+    const supabase = createServerClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        {
+            cookies: {
+                getAll() {
+                    return request.cookies.getAll();
+                },
+                setAll(
+                    cookiesToSet: {
+                        name: string;
+                        value: string;
+                        options?: Record<string, unknown>;
+                    }[]
+                ) {
+                    cookiesToSet.forEach(({ name, value, options }) =>
+                        request.cookies.set(name, value)
+                    );
+                    supabaseResponse = NextResponse.next({
+                        request,
+                    });
+                    cookiesToSet.forEach(({ name, value, options }) =>
+                        supabaseResponse.cookies.set(name, value, options)
+                    );
+                },
             },
-            setAll(cookiesToSet) {
-                cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value));
-                supabaseResponse = NextResponse.next({
-                    request,
-                });
-                cookiesToSet.forEach(({ name, value, options }) => supabaseResponse.cookies.set(name, value, options));
-            },
-        },
-    });
+        }
+    );
 
     // IMPORTANT: DO NOT ADD CODE HERE OR REMOVE auth.getUser()
 
