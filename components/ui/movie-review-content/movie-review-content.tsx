48e1fc6c8d93656eff4373a3f07fcb202b934f88
Andrew Fisher <fisherandrew777@gmail.com>
2026-07-02 00:41:14 -0600

    Work on rendering stars

--- components/ui/movie-review-content/movie-review-content.tsx
+++ components/ui/movie-review-content/movie-review-content.tsx
@@ -1,4 +1,4 @@
-import { H3, Text } from 'thread-ui';
+import { H3, Icon, Text } from 'thread-ui';
 
 type MovieReviewContentProps = {
     title: string;
@@ -6,14 +6,36 @@ type MovieReviewContentProps = {
     review: string;
 };
 
+const STAR_COUNT = 5;
+
 export const MovieReviewContent = ({ title, stars, review }: MovieReviewContentProps) => {
-    const starList = '★★★★★';
+    const half = stars % 1;
+    const starList = [];
+
+    for (let i = 1; i < stars; i++) {
+        console.log('Full', i);
+
+        starList.push(<Icon filled key={i} name="Star" size={8} />);
+    }
+    if (half > 0) {
+        starList.push(<Icon filled key={half} name="StarHalf" size={8} />);
+    }
+    for (let i = stars + 1; i < STAR_COUNT; i++) {
+        console.log('Empty', i);
+        starList.push(<Icon key={i} name="Star" size={8} />);
+    }
 
     return (
         <>
-            <H3 inline>{title}</H3>
-            <Text>{starList}</Text>
-            <Text inline>{review}</Text>
+            <H3 color="white" inline>
+                {title}
+            </H3>
+            <Text color="white" inline>
+                <div className="flex flex-row">{starList}</div>
+            </Text>
+            <Text color="white" inline>
+                {review}
+            </Text>
         </>
     );
 };
