a525751d54fbd0232bcfdfe90ef5e74efc443e38
Andrew Fisher <fisherandrew777@gmail.com>
2026-07-02 00:51:43 -0600

    Simplify star rendering

--- components/ui/movie-review-content/movie-review-content.tsx
+++ components/ui/movie-review-content/movie-review-content.tsx
@@ -1,4 +1,4 @@
-import { H3, Icon, Text } from 'thread-ui';
+import { H3, Icon, Text, ThreadTheme } from 'thread-ui';
 
 type MovieReviewContentProps = {
     title: string;
@@ -9,28 +9,20 @@ type MovieReviewContentProps = {
 const STAR_COUNT = 5;
 
 export const MovieReviewContent = ({ title, stars, review }: MovieReviewContentProps) => {
-    const half = stars % 1;
-    const whole = stars - half;
-    const starList = [];
-
-    for (let i = 0; i < STAR_COUNT; i++) {
-        if (i < whole) {
-            starList.push(<Icon filled key={i} name="Star" size={8} />);
-        } else if (i < whole + 1 && half > 0) {
-            starList.push(<Icon filled key={half} name="StarHalf" size={8} />);
-        } else {
-            starList.push(<Icon key={i} name="Star" size={8} />);
-        }
-    }
+    const stars_ = Array.from({ length: STAR_COUNT }, (_, i) => {
+        const filled = i < Math.floor(stars);
+        const half = !filled && i < stars;
+        return <Icon filled={filled || half} key={i} name={half ? 'StarHalf' : 'Star'} size={8} />;
+    });
 
     return (
         <>
             <H3 color="white" inline>
                 {title}
             </H3>
-            <Text color="white" inline>
-                <div className="flex flex-row">{starList}</div>
-            </Text>
+            <div style={{ color: ThreadTheme.white }} className="flex flex-row">
+                {stars_}
+            </div>
             <Text color="white" inline>
                 {review}
             </Text>
