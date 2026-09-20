3f36dbb6cd75514bc9d9bc2cca4e6593be38b4f4
Andrew Fisher <fisherandrew777@gmail.com>
2026-09-20 17:44:50 -0600

    Refactor Artist type

--- types/models/artist/artist.types.ts
+++ types/models/artist/artist.types.ts
@@ -1,21 +1,25 @@
-import { Attribute, AttributeDto, Content, ContentDto, Image, ImageDto, Link, LinkDto } from '@/types';
+import {
+    Album,
+    Attribute,
+    AttributeDto,
+    Content,
+    ContentDto,
+    Image,
+    ImageDto,
+    Link,
+    LinkDto,
+    MusicItem,
+} from '@/types';
 import { DtoId } from '../dto';
+import { Prettify } from 'thread-ui';
 
-export type Artist = {
-    id: number;
-    name: string;
-    tier: number;
-    rank?: number;
-    contents: Content[];
-    attributes: Attribute[];
-    link: Link;
-    image: Image;
-};
-
-export type ArtistDto = Omit<Artist, 'id' | 'contents' | 'attributes' | 'link' | 'image'> & {
-    id: DtoId;
-    contents: ContentDto[];
-    attributes: AttributeDto[];
-    link: LinkDto;
-    image: ImageDto;
-};
+export type Artist = Prettify<
+    {
+        id: number;
+        name: string;
+        contents: string[];
+        favoriteTracks: string[];
+        favoriteAlbums: string[];
+        albums?: Album[];
+    } & MusicItem
+>;
