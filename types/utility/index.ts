8355a0fc8201595d5d8d2d0b50f0e5bfbc317feb
Andrew Fisher <fisherandrew777@gmail.com>
2026-09-20 22:22:32 -0600

    Make inputs fully id clean

--- types/utility/index.ts
+++ types/utility/index.ts
@@ -1,5 +1,9 @@
 /** Transform model type into Input Form */
-export type Input<T> = Omit<T, 'id'>;
+export type Input<T> = T extends (infer U)[]
+    ? Input<U>[]
+    : T extends { id: unknown }
+      ? { [K in keyof T as K extends 'id' ? never : K]: Input<T[K]> }
+      : T;
 
 /** Transform model type into Update Form */
 export type Update<T> = Partial<Input<T>> & { id: number };
