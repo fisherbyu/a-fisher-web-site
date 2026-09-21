4051f959c2dbce9b37aae6de26ba44bce54700bf
Andrew Fisher <fisherandrew777@gmail.com>
2026-09-20 18:55:29 -0600

    Add artist ID

--- types/models/album/album.types.ts
+++ types/models/album/album.types.ts
@@ -1,4 +1,4 @@
-import type { MusicItem } from '@/types';
+import type { Artist, MusicItem } from '@/types';
 import type { Prettify } from 'thread-ui';
 
 export type Album = Prettify<
@@ -8,5 +8,6 @@ export type Album = Prettify<
         releaseDate: Date;
         contents: string[];
         favoriteTracks: string[];
+        artistId: Artist['id'];
     } & MusicItem
 >;
