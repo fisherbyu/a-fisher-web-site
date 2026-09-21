079599fc459ca1de60e018f9634cabb4294c6a8e
Andrew Fisher <fisherandrew777@gmail.com>
2026-09-20 23:00:50 -0600

    Refactor detail extraction for albums

--- app/(main)/music/coldplay/contents.tsx
+++ app/(main)/music/coldplay/contents.tsx
@@ -25,40 +25,51 @@ export default function AlbumContents() {
     return (
         <Container>
             <div className="flex flex-col gap-8">
-                {albums?.map((album, _) => (
-                    <MediaCard
-                        key={_}
-                        title={album.name}
-                        description={album.contents.map((content) => content.text)}
-                        details={album.attributes.map((attribute) => ({
-                            title: attribute.title,
-                            details: attribute.text,
-                        }))}
-                        detailsPosition="image"
-                        image={
-                            <Image
-                                src={getPublicUrl(album.image.src)}
-                                alt={album.image.alt}
-                                height={album.image.height}
-                                width={album.image.width}
-                            />
-                        }
-                        imagePosition={_ % 2 === 0 ? 'right' : 'left'}
-                        size="lg"
-                        links={[
-                            <Link href={getMusicLink(album.link.appleURI, 'album', 'apple')}>
+                {albums?.map((album, _) => {
+                    const releaseYear = album.releaseDate?.getFullYear();
+
+                    const details = [
+                        {
+                            title: `Favorite Track${album.favoriteTracks.length > 1 ? 's' : ''}:`,
+                            details: album.favoriteTracks.join(', '),
+                        },
+                        ...(releaseYear ? [{ title: 'Year:', details: String(releaseYear) }] : []),
+                    ];
+
+                    return (
+                        <MediaCard
+                            key={_}
+                            title={album.title}
+                            description={album.contents.map((content) => content)}
+                            details={details}
+                            detailsPosition="image"
+                            image={
                                 <Image
-                                    className=" w-5"
-                                    src={AppleMusicLogo}
-                                    alt="Logo, Apple Music"
+                                    src={getPublicUrl(album.image.src)}
+                                    alt={album.image.alt}
+                                    height={album.image.height}
+                                    width={album.image.width}
                                 />
-                            </Link>,
-                            <Link href={getMusicLink(album.link.spotifyURI, 'album', 'spotify')}>
-                                <Image className=" w-5" src={SpotifyLogo} alt="Logo, Spotify" />
-                            </Link>,
-                        ]}
-                    />
-                ))}
+                            }
+                            imagePosition={_ % 2 === 0 ? 'right' : 'left'}
+                            size="lg"
+                            links={[
+                                <Link href={getMusicLink(album.link.appleURI, 'album', 'apple')}>
+                                    <Image
+                                        className=" w-5"
+                                        src={AppleMusicLogo}
+                                        alt="Logo, Apple Music"
+                                    />
+                                </Link>,
+                                <Link
+                                    href={getMusicLink(album.link.spotifyURI, 'album', 'spotify')}
+                                >
+                                    <Image className=" w-5" src={SpotifyLogo} alt="Logo, Spotify" />
+                                </Link>,
+                            ]}
+                        />
+                    );
+                })}
             </div>
         </Container>
     );
