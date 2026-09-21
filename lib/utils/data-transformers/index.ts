1c440a063747beeedc16964f6995718aac615069
Andrew Fisher <fisherandrew777@gmail.com>
2026-09-20 20:43:23 -0600

    export type

--- lib/utils/data-transformers/index.ts
+++ lib/utils/data-transformers/index.ts
@@ -1,4 +1,4 @@
 export { transformAlbum, albumInclude } from './transform-album';
-export { transformArtist } from './transform-artist';
+export { transformArtist, artistInclude } from './transform-artist';
 export { transformPlaylist } from './transform-playlist';
 export { transformRecipe } from './transform-recipe';
