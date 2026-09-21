53b00d16ec7a6d6ba8c1339a8bf3da0ca1059d1b
Andrew Fisher <fisherandrew777@gmail.com>
2026-09-21 14:46:00 -0600

    Refactor login

--- components/features/login-form/login-form.tsx
+++ components/features/login-form/login-form.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState } from 'react';
-import { Button, Divider } from 'thread-ui';
+import { Button, Card, Divider, H3, Text, TextInput } from 'thread-ui';
 import { authenticateUser } from '@/lib';
 import { useRouter } from 'next/navigation';
 
@@ -43,49 +43,18 @@ export const LoginForm = () => {
     };
 
     return (
-        <form
-            onSubmit={handleSubmit}
-            className="sm:w-10/12 md:max-w-80 mx-auto flex flex-col gap-6 justify-center items-center border rounded max-w-screen-sm p-4"
-        >
-            <span>
-                Admin Login
-                <Divider />
-            </span>
-
-            {/* Error message display */}
-            {errorMessage && (
-                <div className="w-full p-3 bg-red-50 text-red-600 border border-red-200 rounded text-sm">{errorMessage}</div>
-            )}
-
-            <label className="flex flex-col gap-2 w-full">
-                Email
-                <input
-                    className="border rounded p-2"
-                    id="email"
-                    name="email"
-                    type="email"
-                    required
-                    minLength={2}
-                    disabled={isLoading}
-                />
-            </label>
-
-            <label className="flex flex-col gap-2 w-full">
-                Password
-                <input
-                    className="border rounded p-2"
-                    id="password"
-                    name="password"
-                    type="password"
-                    required
-                    minLength={2}
-                    disabled={isLoading}
-                />
-            </label>
-
-            <Button fullWidth type="submit" disabled={isLoading}>
-                {isLoading ? 'Logging in...' : 'Login'}
-            </Button>
-        </form>
+        <div className="max-w-96 mx-auto">
+            <Card size="md">
+                <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+                    <H3 inline>Admin Login</H3>
+                    {errorMessage && <Text color="error">{errorMessage}</Text>}
+                    <TextInput name="email" title="Email" type="email" required />
+                    <TextInput name="password" title="Password" type="password" required />
+                    <Button fullWidth type="submit" disabled={isLoading}>
+                        {isLoading ? 'Logging in...' : 'Login'}
+                    </Button>
+                </form>
+            </Card>
+        </div>
     );
 };
