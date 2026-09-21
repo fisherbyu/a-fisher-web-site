ada861ca7177cd8a4ca6972370543c44fe8c0596
Andrew Fisher <fisherandrew777@gmail.com>
2026-09-20 21:20:36 -0600

    Update getter to utilize include

--- lib/actions/data/albums/get-albums.ts
+++ lib/actions/data/albums/get-albums.ts
@@ -1,23 +1,10 @@
 'use server';
-import { prisma, transformAlbum } from '@/lib';
+import { prisma, transformAlbum, albumInclude } from '@/lib';
 import { Album } from '@/types';
 
 export const getAlbums = async (): Promise<Album[]> => {
     const data = await prisma.album.findMany({
-        include: {
-            link: true,
-            image: true,
-            contents: {
-                orderBy: {
-                    order: 'asc',
-                },
-            },
-            attributes: {
-                orderBy: {
-                    order: 'asc',
-                },
-            },
-        },
+        include: albumInclude,
     });
 
     return data.map(transformAlbum);
--- lib/actions/data/artist/get-artists.ts
+++ lib/actions/data/artist/get-artists.ts
@@ -1,24 +1,11 @@
 'use server';
 import { prisma } from '@/lib/prisma';
-import { transformArtist } from '@/lib';
+import { transformArtist, artistInclude } from '@/lib';
 import { Artist } from '@/types';
 
 export const getArtists = async (): Promise<Artist[]> => {
     const data = await prisma.artist.findMany({
-        include: {
-            link: true,
-            image: true,
-            contents: {
-                orderBy: {
-                    order: 'asc',
-                },
-            },
-            attributes: {
-                orderBy: {
-                    order: 'asc',
-                },
-            },
-        },
+        include: artistInclude,
     });
 
     return data.map(transformArtist);
