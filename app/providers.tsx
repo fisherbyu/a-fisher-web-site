6561b299d30180fc38d92bf07930fc3abf708140
Andrew Fisher <fisherandrew777@gmail.com>
2026-09-23 13:54:53 -0600

    Configure global fetcher

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,13 @@
 'use client';
 import { ThreadProvider } from 'thread-ui';
 import Link from 'next/link';
+import { SWRConfig } from 'swr';
+import { makeRequest } from '@/lib';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-    return <ThreadProvider linkComponent={Link}>{children}</ThreadProvider>;
+    return (
+        <ThreadProvider linkComponent={Link}>
+            <SWRConfig value={{ fetcher: makeRequest }}>{children}</SWRConfig>
+        </ThreadProvider>
+    );
 }
