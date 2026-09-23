af1f65fd4d497557b10b5ded83dc8ecc560d475d
Andrew Fisher <fisherandrew777@gmail.com>
2026-09-23 13:58:39 -0600

    Update import

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,5 @@
 import type { NextRequest } from 'next/server';
-import { updateSession } from '@/lib';
+import { updateSession } from './server';
 
 export async function proxy(request: NextRequest) {
     return await updateSession(request);
