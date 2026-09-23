01c318de60f0ce3f887b03b590223df5da0ad1e3
Andrew Fisher <fisherandrew777@gmail.com>
2026-09-23 14:48:36 -0600

    Move auth actions to auth dir

--- server/auth/auth.actions.ts
+++ server/auth/auth.actions.ts
@@ -1,5 +1,5 @@
 'use server';
-import { createSupabaseServerClient } from './clients';
+import { createSupabaseServerClient } from '../clients';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
